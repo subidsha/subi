54ce94d26a2ff15546819b9921f553ce9fa2e426
subidsha <31504698+subidsha@users.noreply.github.com>
2017-09-22 20:56:24 +0530

    Create alpha.c

---
+++ alpha.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string`h>
+{
+char s;
+printf("enter the letter",s);
+scanf("%c",s);
+if(s>a&&s<z)
+{
+printf(" the letter is alphapet",s);
+}
+else
+{
+printf("the letter isnot alphapet",s);
+}
+}
